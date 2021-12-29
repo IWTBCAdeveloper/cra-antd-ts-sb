14a989d191c7e977a483955636dd840fdf7c80d0
VinhNguyen <vinhnguyenvan1995@gmail.com>
2021-12-29 22:11:44 +0700

    feat: init

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -26,10 +26,7 @@ export function configureAppStore() {
   const store = configureStore({
     reducer: createReducer(),
     middleware: [...getDefaultMiddleware(), ...middlewares],
-    devTools:
-      /* istanbul ignore next line */
-      process.env.NODE_ENV !== 'production' ||
-      process.env.PUBLIC_URL.length > 0,
+    devTools: process.env.NODE_ENV !== 'production',
     enhancers,
   });
 
