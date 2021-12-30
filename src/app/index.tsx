6b5b586382c90e8d71903cd54cc0de145a4624ce
VinhNguyen <vinhnguyenvan1995@gmail.com>
2021-12-30 11:53:47 +0700

    feat: add antd

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -9,12 +9,13 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import 'antd/dist/antd.min.css';
 
 import { GlobalStyle } from 'styles/global-styles';
 
 import { HomePage } from './pages/HomePage/Loadable';
 import { NotFoundPage } from './components/NotFoundPage/Loadable';
-import { useTranslation } from 'react-i18next';
 
 export function App() {
   const { i18n } = useTranslation();
--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
+import { Button } from 'antd';
 
 export function HomePage() {
   return (
@@ -8,6 +9,7 @@ export function HomePage() {
         <title>Home Page</title>
         <meta name="description" content="A Boilerplate application homepage" />
       </Helmet>
+      <Button type="primary">Button</Button>
       <span>{process.env.REACT_APP_NOT_SECRET_CODE}</span>
     </>
   );
